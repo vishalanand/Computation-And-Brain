8601551d762910040e4453acb1cda3387e04b747
Vishal Anand <anand.vishal2994@gmail.com>
2018-11-19 23:10:52 -0500

    Verbose

--- setup_env/execute.sh
+++ setup_env/execute.sh
@@ -1,8 +1,16 @@
+#First time execution start
 python3 -m venv env
-source env/bin/activate
 pip install --upgrade pip
+source env/bin/activate
+pip install -r requirements.txt
+deactivate
+#First time execution end
+
+#Each time code is revisited start
+source activate env/bin
 #conda install pytorch torchvision -c pytorch
 #pip3 install torch torchvision
 pip install -r requirements.txt
 deactivate
+#Each time code is revisited end
 
