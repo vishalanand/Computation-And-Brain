b3db4ec0bf2a0319f3d52be314faa24eff21bc08
Vishal Anand <anand.vishal2994@gmail.com>
2018-11-19 22:52:07 -0500

    Change Execute for pip

--- execute.sh
+++ execute.sh
@@ -1,6 +1,7 @@
 python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
-conda install pytorch torchvision -c pytorch
+#conda install pytorch torchvision -c pytorch
+pip3 install torch torchvision
 deactivate
 
